65b0bd42411ed5c7f9efc3ec867181200b1216f9
theray <info@fuse.sk>
2012-07-26 22:23:28 +0200

    new comm

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>designplace.org search script</title>
+<meta name="author" content="Steve R, http://www.designplace.org/">
+</head>
+<!-- © http://www.designplace.org/ -->
+<body>
+
+<form method='post'>
+Meno NPC = <input type='text' name='name' />
+<input type='submit' value='submit' />
+</form>
+
+<?
+mysql_connect ("localhost", "root","root")  or die (mysql_error());
+mysql_select_db ("world");
+
+if (isset($_POST['name']))
+{
+$result=mysql_query("SELECT * FROM `creature_template` WHERE `name`='".$_POST['name']."'");
+echo "<table border=0 cellpadding=4 cellspacing=0><tr><td>id</td><td>gid</td><td>column3</td></tr>";
+while ($rowid=mysql_fetch_array($result))
+{
+//perform actions for earch row found
+echo "Meno:".$rowid['name']."</br>";
+echo "ID:".$rowid['entry']."</br>";
+echo "Model ID:".$rowid['modelid1']."</br>";
+echo 'Link: <a href="http://localhost/fuse.sk/database/creature_template/'.$rowid['entry'].'/index.php?id='.$rowid['entry'].'">    http://localhost/fuse.sk/database/creature_template/'.$rowid['entry'].'/index.php?id='.$rowid['entry'].'</a>';
+}
+echo "</table>";
+}
+?>
+
+</body>
+</html>
